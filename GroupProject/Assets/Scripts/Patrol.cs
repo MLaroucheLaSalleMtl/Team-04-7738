fd511db2e1188c0566e0ff60c4a02452a13e9f54
Nicolas <derekhasun@gmail.com>
2019-02-20 13:23:26 -0500

    Ghost Enemy
    Add first enemy

--- GroupProject/Assets/Scripts/Patrol.cs
+++ GroupProject/Assets/Scripts/Patrol.cs
@@ -18,16 +18,18 @@ public class Patrol : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 1f);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 0.3f);
         if (groundInfo.collider == false) {
             if (movingRight == true)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 movingRight = false;
+
             }
             else {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 movingRight = true;
+
             }
         }
     }
