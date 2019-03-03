e01c8a11017d2946ed50c624319d3922ef6b13c2
Nicolas <derekhasun@gmail.com>
2019-03-03 14:51:37 -0500

    Add Main Menu and In Game Menu Script

---
+++ GroupProject/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    bool Pressed = false;
+    public GameObject Menu;
+
+    void Update()
+    {
+        if (Pressed == false && Input.GetAxisRaw("Cancel") > 0)
+        {
+            Pressed = true;
+            if (Menu.activeInHierarchy == false)
+            {
+                SetOn();
+            }
+            else
+            {
+                SetOff();
+            }
+
+        }
+        else if (Pressed == true && Input.GetAxisRaw("Cancel") == 0)
+        {
+            Pressed = false;
+        }
+    }
+    public void SetOn()
+    {
+        Time.timeScale = 0;
+        Menu.SetActive(true);
+    }
+
+    public void SetOff()
+    {
+        Time.timeScale = 1;
+        Menu.SetActive(false);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ GroupProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private AsyncOperation async;
+
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PlayGame()
+    {
+        if (async == null)
+        {
+            Scene currScene = SceneManager.GetActiveScene();
+            async = SceneManager.LoadSceneAsync(currScene.buildIndex + 1);
+            async.allowSceneActivation = true;
+        }
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
